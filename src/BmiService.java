846025d7f84ce6ee3800e1512a36d5d1a77f7910
Dmitrii Andreev <andreev352@gmail.com>
2022-12-03 12:13:19 +0300

    initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public float calculate(float a, float b) {
+
+        float result = (a / (b * b));
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float a = 70; // вес указать в кг.
+        float b = 1.70f; //рост указать в метрах.
+
+        BmiService service = new BmiService();
+        float myCalculate = service.calculate(a , b);
+
+
+        System.out.println("индекс массы тела равен: " + myCalculate + " кг/м²");
+
+    }
+}
