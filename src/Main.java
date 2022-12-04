4c199922a8befc196c2ad329258a93b9c9ee2b2f
DinoBambinio <117571267+DinoBambinio@users.noreply.github.com>
2022-12-04 10:49:07 +0300

    added term

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        float a = 70; // вес указать в кг.
-        float b = 1.70f; //рост указать в метрах.
+        float weight = 70; // вес указать в кг.
+        float height = 1.70f; //рост указать в метрах.
 
         BmiService service = new BmiService();
-        float myCalculate = service.calculate(a, b);
+        float myCalculate = service.calculate(weight, height);
 
 
         System.out.println("индекс массы тела равен: " + myCalculate + " кг/м²");
